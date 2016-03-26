0dec4e49b22efe4f0b211c47fc95392b6de3f552
Filipski <afilipski@constantcontact.com>
2016-03-25 23:59:48 -0400

    Got one string to change based on tuning

--- scale_helper.js
+++ scale_helper.js
@@ -1,8 +1,18 @@
+Notes = new Meteor.Collection('notes');
+
 if (Meteor.isClient) {
-  // globals for note and mode
-  var data = {};
+  
+  // globals defaults 
+  var data = {note:"a", mode:"ionian", tuning:"estandard"};
+  var tuning = {
+    dstandard:['D','A','F','C','G','D'],
+    estandard:['E','B','G','D','A','E']
+  };
+
+  // TODO: investigate what this does
   var tracker = new Tracker.Dependency();
 
+  // Template variables inside of template demo
   Template.demo.helpers({
     note: function () {
       tracker.depend();
@@ -11,17 +21,34 @@ if (Meteor.isClient) {
     mode: function () {
       tracker.depend();
       return data.mode;
+    },
+    tuning: function () {
+      tracker.depend();
+      return data.tuning;
     }
   });
 
+  // Template variables inside of template fretbase
+  Template.fretbase.helpers({
+    name: function() {
+      tracker.depend();
+      return tuning[data.tuning][parseInt(this.string)];
+    }
+  });
+
+  // Listens to all events on body
   Template.body.events({
+
+    // From the form scale-creator
     "submit .scale-creator": function (event) {
+
       // Prevent default browser form submit
       event.preventDefault();
- 
+
       // Get value from form element
       data.note = event.target.note.value;
       data.mode = event.target.mode.value;
+      data.tuning = event.target.tuning.value;
 
       tracker.changed();
     }
